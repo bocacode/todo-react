3dbeac344048e3b32722aaf153f09243d81649be
jonathans199 <jso1919@gmail.com>
2021-02-11 17:13:17 -0500

    added items in todos array

--- src/Todo.js
+++ src/Todo.js
@@ -1,22 +1,61 @@
 import React, { useState } from 'react'
+import SingleTodo from './todo/SingleTodo'
 
 const Todo = () => {
-  const [todos, setTodos] = useState(['first todo', 'second todo', 'third todo'])
+  const [todos, setTodos] = useState([])
+  const [inputItem, setInputItem] = useState()
+
+  const handleSubmit = () => {
+    setTodos([...todos, inputItem])
+    
+  }
+
+  console.log('todos ===> ', todos)
+  
+  // not in use
+  function handleInput(e) {
+    console.log('while typing =>', e.target.value)
+
+    setInputItem(e.target.value)
+
+    console.log('in state => ', inputItem)
+  }
 
   return (
     <>
-      <h1> todo List</h1>
-      {todos.map((x, i) => {
-        console.log('item', x, '', i)
+      <form>
+        <div className='form-group'>
+          <input
+            type='text'
+            className='form-control'
+            id='exampleFormControlInput1'
+            placeholder='new Todo'
+            // onChange={e => handleInput(e)} // non sexy way
+            onChange={e => {
+              setInputItem(e.target.value)
+              // console.log(e.target.value)
+            }}
+          />
+          <button
+            type='button'
+            className='btn btn-secondary'
+            onClick={handleSubmit} // this is the almost the same - getting event
+            // onClick={(event) => handleSubmit(event)} // equivalent to top
+          >
+            add todo
+          </button>
+        </div>
+      </form>
+
+      {/* {todos.map((x, i) => {
+        // console.log('item', x, '', i)
 
         return (
           <>
-            <h1 className='text-muted'> this is just HTML </h1>
-            <h2 className='text-warning'>here is an item {x}</h2>
-            <h2 className='text-danger'>this is just x {x}</h2>
+            <SingleTodo />
           </>
         )
-      })}
+      })} */}
     </>
   )
 }
---
+++ src/todo/SingleTodo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SingleTodo() {
+  return (
+    <>
+      <h1> Single todo</h1>
+    </>
+  )
+}
+
+export default SingleTodo
